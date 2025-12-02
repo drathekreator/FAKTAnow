3632416c802ecf75d36b61d1bcb395f6403e1238
drathekreator <anomalindra@apps.ipb.ac.id>
2025-12-02 19:50:50 +0700

    ubah homepage dikit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home</title>
-    @vite('resources/css/app.css')
+    <title>{{ $title ?? 'FAKTAnow'}}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <header>
+<body class="tex-gray-900">
+    <header class="border-b bg-white">
+        
         <!-- Navbar -->
     </header>
 
