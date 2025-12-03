a972ac3909b06609615e7a591e724ff22ac3518f
drathekreator <anomalindra@apps.ipb.ac.id>
2025-12-03 16:09:28 +0700

    menambahkan homepage to all users

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Routing\Router; // <-- Wajib di-import
 use App\Http\Middleware\CheckUserRole; // <-- Middleware kustom Anda
+use App\Http\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,6 +34,11 @@ class AppServiceProvider extends ServiceProvider
         // Mendaftarkan alias 'role' agar bisa digunakan di route: middleware('role:admin')
         $router->aliasMiddleware('role', CheckUserRole::class);
         
-        // =======================================================
+        // ====================================================
+        // 
+        // View::composer('*', function($view){
+        //     $view->with('categories', Categories::all());
+        // });
+
     }
 }
